0ca0c5aebccea110cfc608a131979d8dd5e49560
Agustin Tamagno <agustin.tamagno@qualityconnex.com>
2017-11-02 13:03:17 +1100

    testing back to production

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -2,7 +2,7 @@
 
 angular.module('core').config(
 	function($stateProvider, $urlRouterProvider) {
-		//$urlRouterProvider.otherwise('/connect');
+		/*
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider.
@@ -34,8 +34,10 @@ angular.module('core').config(
 				url: '/',
 				templateUrl: 'modules/core/views/main.client.view.html'
 			});
-
-		/*
+		*/
+		
+		$urlRouterProvider.otherwise('/connect');
+		
 		$stateProvider.
 			state('home', {
 				url: '/',
@@ -65,5 +67,4 @@ angular.module('core').config(
 				url: '^/video',
 				templateUrl: 'modules/core/views/video.client.view.html'
 			});
-			*/
 	});
